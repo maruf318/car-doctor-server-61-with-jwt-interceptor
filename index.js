8d9eb4e0b569dd4975f9d20255bf9e18c2e13ab1
Maruf Hossain <hossainmaruf001@gmail.com>
2023-10-31 21:09:06 +0100

    jwt and interceptor added

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://cars-doctor-e56c7.web.app",
+      "https://cars-doctor-e56c7.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
@@ -31,26 +35,24 @@ const client = new MongoClient(uri, {
 });
 
 //our middlewares
-const logger = async (req, res, next) => {
-  console.log("called", req.host, req.originalUrl);
+const logger = (req, res, next) => {
+  console.log("log info:", req.method, req.url);
   next();
 };
 
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token;
-  console.log("value of token in middleware", token);
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log("token in the middleware", token);
+
   if (!token) {
-    return res.status(401).send({ message: "unauthorized" });
+    return res.status(401).send({ message: "unauthorized access" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    //error
     if (err) {
-      console.log(err);
-      return res.status(401).send("unauthorized");
+      return res.status(401).send({ message: "unauthorized access" });
     }
-    //if token is valid then it would be decoded
-    console.log("value in the token", decoded);
     req.user = decoded;
+    // console.log(decodedValue);
     next();
   });
 };
@@ -58,7 +60,7 @@ const verifyToken = async (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const serviceCollection = client.db("carData").collection("services");
     const bookingCheckoutCollection = client
       .db("carData")
@@ -67,22 +69,25 @@ async function run() {
     //auth related api
     app.post("/jwt", logger, async (req, res) => {
       const user = req.body;
-      console.log(user);
+      console.log("user for token", user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false, //http://localhost:5173/login
-          // sameSite: "none", //client site and server site is different
+          secure: true,
+          sameSite: "none",
         })
-        // .send(token);
         .send({ success: true });
     });
-
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logout user:", user);
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
     //services related api
-    app.get("/services", logger, async (req, res) => {
+    app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -103,10 +108,12 @@ async function run() {
     app.get("/bookings", logger, verifyToken, async (req, res) => {
       // console.log(req.query);
       console.log(req.query.email);
-      console.log("user in the valid token", req.user);
-      if (req.query.email !== req.user.email) {
+      // console.log("cook cook cookies in booking", req.cookies);
+      console.log("token owner info", req.user);
+      if (req.user.email !== req.query.email) {
         return res.status(403).send({ message: "forbidden access" });
       }
+
       // console.log("tok tok my token", req.cookies.token);
       let query = {};
       if (req.query?.email) {
@@ -146,7 +153,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
